a599de4e44df2d5a652ffe56ac08e1b0b220857c
shivendra124 <shivendra124124@gmail.com>
2020-03-31 11:27:47 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    i = ((tip_percent/100)*meal_cost)
+    j = ((tax_percent/100)*meal_cost)
+    print(round(i+j+meal_cost))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
