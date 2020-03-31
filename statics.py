71f6d7aa3c1e553c6c51cdaf0acfb2524cb6e83c
shivendra124 <shivendra124124@gmail.com>
2020-03-31 14:49:54 +0530

    statics

---
+++ statics.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+n = int(input())
+
+lst = list((map(int,input().split(" "))))
+    
+mean = sum(lst)/n
+lst.sort()
+median = None
+if n%2==0:
+    median = (lst[int(len(lst)/2-1)]+lst[int(len(lst)/2)])/2
+else:
+    median = round(len(lst)/2)    
+res = Counter(lst)
+mode = res.most_common(1)[0][0]
+
+print(mean)
+print(median)
+print(mode)
