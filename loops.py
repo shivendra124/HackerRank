400c61044b6a25fb2d4cb4ca47c3c16c5df118f1
shivendra124 <shivendra124124@gmail.com>
2020-04-01 13:27:34 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n = int(input().strip())
+    if n%2 == 0:
+        if n in range(2,6) or n > 20:
+            print("Not Weird")
+        elif n in range(6,21):
+            print("Weird")
+    else:
+        print("Weird")
